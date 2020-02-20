b082baf3a0fefc755aa6cc7f78b710944cacae41
Natalia_mazhenkova <zjecutiez@gmail.com>
2020-02-20 14:11:25 +0300

    Скрипты для нового плеера

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 // import * as util from "./utils/utils";
 import * as pleer from "./utils/pleer";
+import('./utils/audioAdd.js');
+import('./utils/pleerStyle.js');
 //alert("Hello World");
 // util.fetchAPI();
 
